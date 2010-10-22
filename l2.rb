7b4d2ef5cf8a0e650011ac68c72c1a00d517cf93
baalberith <baal6erith@gmail.com>
2010-10-22 09:00:58 +0200

    lista 2

--- l2.rb
+++ l2.rb
@@ -1,39 +1,74 @@
 # zadanie 1
 
-map = { 
-    "Polska" => ["Niemcy", "Czechy", "Slowacja"],
-    "Niemcy" => ["Francja", "Szwajcaria", "Austria", "Czechy", "Polska"],
-    "Czechy" => ["Niemcy", "Austria", "Wegry", "Slowacja"],
-    "Slowacja" => ["Czechy", "Austria", "Wegry"],
-    "Francja" => ["Niemcy", "Szwajcaria", "Wlochy"],
-    "Szwajcaria" => ["Niemcy", "Wlochy", "Austria"]
-}
-
-def all_paths(graph, a, b, path = [])   
-    path += [a]
-    
-    if a == b
-        return [path]
+class Graph
+    def initialize
+        @g = {}
     end
     
-    if not graph.has_key?(a)
-        return []
-    end
-    
-    paths = []
-    
-    graph[a].each do |n|
-        if not path.include?(n)
-            newpaths = all_paths(graph, n, b, path)
-            newpaths.each do |newpath|
-                paths << newpath
-            end
+    def add_edge(a, b)
+        if not @g.has_key?(a)
+            @g[a] = [b]       
+        else
+            @g[a] << b  
+        end
+        
+        if not @g.has_key?(b)
+            @g[b] = [a]       
+        else
+            @g[b] << a  
         end
     end
     
-    return paths
+    def find_paths(a, b, path = [])   
+        path += [a]
+    
+        if a == b
+            return [path]
+        end
+    
+        if not @g.has_key?(a)
+            return []
+        end
+    
+        paths = []
+    
+        @g[a].each do |n|
+            if not path.include?(n)
+                newpaths = find_paths(n, b, path)
+                newpaths.each do |newpath|
+                    paths << newpath
+                end
+            end
+        end
+    
+        return paths
+    end
 end
 
-all_paths(map, "Polska", "Wlochy").each do |path|
+
+map = Graph.new
+
+map.add_edge("Polska", "Niemcy")
+map.add_edge("Polska", "Czechy")
+map.add_edge("Polska", "Slowacja")
+map.add_edge("Niemcy", "Francja")
+map.add_edge("Niemcy", "Szwajcaria")
+map.add_edge("Niemcy", "Austria")
+map.add_edge("Niemcy", "Czechy")
+map.add_edge("Czechy", "Austria")
+map.add_edge("Czechy", "Wegry")
+map.add_edge("Czechy", "Slowacja")
+map.add_edge("Slowacja", "Austria")
+map.add_edge("Slowacja", "Wegry")
+map.add_edge("Francja", "Szwajcaria")
+map.add_edge("Francja", "Wlochy")
+map.add_edge("Szwajcaria", "Wlochy")
+map.add_edge("Szwajcaria", "Austria")
+
+
+map.find_paths("Polska", "Wlochy").each do |path|
     p path
 end
+
+puts "Najkrotsza droga z Polski do Wloch: "
+p map.find_paths("Polska", "Wlochy").sort_by { |path| path.length }.first
